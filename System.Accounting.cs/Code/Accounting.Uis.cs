fad5334c8c4349274a1610f8c376d61731cc0f73
mohsen hadaeghi <hadaeghimohsen@yahoo.com>
2018-10-04 15:40:41 +0330

    Update

---
+++ System.Accounting.cs/Code/Accounting.Uis.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.JobRouting.Routering;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System.Accounting.Code
+{
+   partial class Accounting
+   {
+      public ISendRequest _Wall { get; set; }
+
+      //internal Ui.RTL.MasterPage.FRST_PAGE_F _Frst_Page_F { get; set; }
+   }
+}
---
+++ System.Accounting.cs/ExceptionHandlings/ExceptionLogs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+
+namespace System.Accounting.ExceptionHandlings
+{
+   public static class ExceptionLogs
+   {
+      //public static void SaveException(this CRM.Data.iCRMDataContext iCRM, Exception e)
+      //{
+      //   //MessageBox.Show(e.Message);
+      //}
+   }
+}

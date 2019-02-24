ec9b8960ef7bce6ee5f908a9a1bac4d41b4a830f
mohsen hadaeghi <hadaeghimohsen@yahoo.com>
2019-02-25 01:24:45 +0330

    update

--- System.Scsc/Ui/MasterPage/MAIN_PAGE_F.Designer.cs
+++ System.Scsc/Ui/MasterPage/MAIN_PAGE_F.Designer.cs
@@ -1814,7 +1814,6 @@
          this.button4.TabIndex = 21;
          this.button4.Text = "Empty";
          this.button4.UseVisualStyleBackColor = true;
-         this.button4.Visible = false;
          this.button4.Click += new System.EventHandler(this.button4_Click);
          // 
          // button3
